6c8096d6b7e72decdd29d74834bf92f157bff48f
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-10-10 21:31:11 +0300

    feat: added new navigation route

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import './providers/cart.dart';
 import './providers/orders.dart';
+import './providers/products_provider.dart';
 import './screens/cart_screen.dart';
 import './screens/orders_screen.dart';
 import './screens/user_products_screen.dart';
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
-import './providers/products_provider.dart';
+import './screens/edit_product_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -41,6 +42,7 @@ class MyApp extends StatelessWidget {
           CartScreen.routeName: (context) => CartScreen(),
           OrdersScreen.routeName: (context) => OrdersScreen(),
           UserProductsScreen.routeName: (context) => UserProductsScreen(),
+          EditProductScreen.routeName: (context) => EditProductScreen(),
         },
       ),
     );
