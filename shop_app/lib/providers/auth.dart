015796b8c2bd04d17ebbc88d83e01eaf2ad0a01b
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-10-17 17:21:31 +0300

    refactor: rebuild post method to work with login or singup requests

--- shop_app/lib/providers/auth.dart
+++ shop_app/lib/providers/auth.dart
@@ -8,13 +8,23 @@ class Auth with ChangeNotifier {
   DateTime _expiryDate;
   String _userId;
 
-  Future<void> signup(String email, String password) async {
-    // https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
     final url = Uri.parse(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA1WJi7syZuFSqR2p34L3WPCAQhgEhkYyo");
+        "https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyA1WJi7syZuFSqR2p34L3WPCAQhgEhkYyo");
     final response = await http.post(url,
         body: json.encode(
             {"email": email, "password": password, "returnSecureToken": true}));
     print(json.decode(response.body));
+  }
+
+  Future<void> signup(String email, String password) async {
+    // https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
+    return _authenticate(email, password, "signUp");
+  }
+
+  Future<void> login(String email, String password) async {
+    // https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]
+    return _authenticate(email, password, "signInWithPassword");
   }
 }
