db77df11dbd2e63a5d1cce3520b703c5c050c43e
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-10-17 23:41:58 +0300

    feat: added new logic for singup, login and error

--- shop_app/lib/providers/auth.dart
+++ shop_app/lib/providers/auth.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
-
-import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter/widgets.dart';
+import '../models/http_exception.dart';
 
 class Auth with ChangeNotifier {
   String _token;
@@ -12,10 +12,22 @@ class Auth with ChangeNotifier {
       String email, String password, String urlSegment) async {
     final url = Uri.parse(
         "https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyA1WJi7syZuFSqR2p34L3WPCAQhgEhkYyo");
-    final response = await http.post(url,
-        body: json.encode(
-            {"email": email, "password": password, "returnSecureToken": true}));
-    print(json.decode(response.body));
+
+    try {
+      final response = await http.post(url,
+          body: json.encode({
+            "email": email,
+            "password": password,
+            "returnSecureToken": true
+          }));
+      print(json.decode(response.body));
+      final responseData = json.decode(response.body);
+      if (responseData["error"] != null) {
+        throw HttpException(responseData["error"]["message"]);
+      }
+    } catch (error) {
+      throw error;
+    }
   }
 
   Future<void> signup(String email, String password) async {
