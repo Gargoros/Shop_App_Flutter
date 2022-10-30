3a11720a11776d5bb87b19fba190bc4658178390
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-10-30 23:01:20 +0300

    feat: init new folder and new animated method for images

--- shop_app/lib/widgets/product_item.dart
+++ shop_app/lib/widgets/product_item.dart
@@ -15,15 +15,15 @@ class ProductItem extends StatelessWidget {
       borderRadius: BorderRadius.circular(12),
       child: GridTile(
         child: GestureDetector(
-          onTap: () {
-            Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
-                arguments: product.id);
-          },
-          child: Image.network(
-            product.imageUrl,
-            fit: BoxFit.cover,
-          ),
-        ),
+            onTap: () {
+              Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
+                  arguments: product.id);
+            },
+            child: FadeInImage(
+              placeholder: AssetImage("assets/images/product-placeholder.png"),
+              image: NetworkImage(product.imageUrl),
+              fit: BoxFit.cover,
+            )),
         footer: GridTileBar(
             leading: Consumer<Product>(
               builder: (ctx, product, _) => IconButton(
